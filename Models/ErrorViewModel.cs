f3b4209daf8f7b5c89c750f9b874d1a2e8249ef6
Sai Nitish Grandhi <nitishgrandhi222@gmail.com>
2021-09-08 18:54:57 +0530

    descriptive comment for code changes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstWebApi.dll.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
